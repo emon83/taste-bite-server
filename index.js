61f6ee7cc5f9c1dd6caadff59f862bdf0f02c601
Emon <emonhasan7650@gmail.com>
2023-05-02 21:12:04 +0600

    install cors and recipeData separate loaded

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 5000
+
+app.use(cors())
+
+const allData = require('./data/chefData.json')
+const recipeData = require('./data/recipeData.json')
 
 app.get('/', (req, res) => {
     res.json({message: "testibite server is running!"})
 })
 
+app.get('/allData', (req, res) =>{
+    res.send(allData);
+})
+
+app.get('/recipeData', (req, res) =>{
+    res.send(recipeData);
+})
+app.get('/recipeData/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedData1 = recipeData.find(data => data.id == id);
+    res.send(selectedData1);
+})
+
+/* app.get('/allData/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedData = allData.find(data => data._id === id);
+    res.send(selectedData);
+}) */
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
