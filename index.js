cd8c6577698ad86fa5e892badc2519db81588292
Emon <emonhasan7650@gmail.com>
2023-05-02 10:22:52 +0600

    install nodemon and basic hello world server setup done

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+    res.json({message: "testibite server is running!"})
+})
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+  })
